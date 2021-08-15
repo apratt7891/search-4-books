a4aef39108000a02dfe2490a12995e15a04e0444
apratt7891 <apratt7891@gmail.com>
2021-08-15 16:04:34 -0600

    schemas

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,56 @@
+// import the qgl tagged template function
+const { gql } = require("apollo-server-express");
+
+// create the typeDefs
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+  type Book {
+    _id: ID
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+  input BookInput {
+    _id: ID
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+  
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(
+      bookId: String!
+      authors: [String]
+      description: String!
+      title: String!
+      image: String!
+      link: String
+    ): User
+    removeBook(bookId: String): User
+  }
+`;
+
+module.exports = typeDefs;
